f9c0c2da12611d3f181dbfea5c2adb4de2fa97ce
Jhonel Villanueva <vvjhonel@gmail.com>
2023-03-30 21:06:21 +0800

    feat: Add requirement to authenticate to all page
    This commit will add funtionality to check if user is authenticated. If
    not, user will be redirected to sign in. This will also include tests
    for the requests

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  root "posts#index"
+  authenticated :user do
+    root to: "posts#index", as: :authenticated_root
+  end
+  root to: redirect("/users/sign_in")
 
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,10 +1,23 @@
 require "rails_helper"
 
 RSpec.describe "Posts" do
-  describe "GET /" do
-    it "returns http success" do
+  context "when user is signed in" do
+    let(:user) { create(:user) }
+
+    before do
+      sign_in user
+    end
+
+    it "renders the root_path" do
       get root_path
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  context "when user is not signed in" do
+    it "redirects to the users sign-in page" do
+      get root_path
+      expect(response).to redirect_to(new_user_session_path)
     end
   end
 end
