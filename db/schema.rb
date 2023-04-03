d218c789656cf21a8c5036e8aeb6750d767407e4
Jhonel Villanueva <vvjhonel@gmail.com>
2023-04-04 05:47:23 +0800

    feat: Add posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_03_125714) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_03_213159) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,6 +39,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_03_125714) do
     t.index ["user_id"], name: "index_notifications_on_user_id"
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username", default: "", null: false
     t.string "email", default: "", null: false
@@ -60,4 +68,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_03_125714) do
   add_foreign_key "friends", "users", column: "friend_id"
   add_foreign_key "notifications", "users"
   add_foreign_key "notifications", "users", column: "sender_id"
+  add_foreign_key "posts", "users"
 end
