5cd38d677f67c0d8624512abd0fa1d8c0c54f19e
Jhonel Villanueva <vvjhonel@gmail.com>
2023-04-07 06:07:25 +0800

    style: Edit the styling of posts and comments

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,5 @@
 module CommentsHelper
+  def get_comment_date(comment)
+    comment.created_at.strftime("%B %d, %Y | %I:%M %P")
+  end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,9 +1,5 @@
 module PostsHelper
   def get_post_date(post)
-    post.created_at.strftime("%B %d, %Y")
-  end
-
-  def get_post_time(post)
-    post.created_at.strftime("%I:%M %P")
+    post.created_at.strftime("%B %d, %Y | %I:%M %P")
   end
 end
