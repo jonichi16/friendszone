5a70dcc61a21818db83781ccc1216c9f9fe849e9
Jhonel Villanueva <vvjhonel@gmail.com>
2023-04-07 14:53:51 +0800

    feat: Add likes model

---
+++ spec/models/like_spec.rb
@@ -0,0 +1,7 @@
+require "rails_helper"
+
+RSpec.describe Like do
+  it { is_expected.to belong_to(:user) }
+  it { is_expected.to belong_to(:post) }
+  it { is_expected.to have_many(:notifications).dependent(:destroy) }
+end
